30a7865667de66b82983d8344e4f798f5c7f4391
Hikari <73871195+Takanashi-Hikari@users.noreply.github.com>
2020-11-03 13:09:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/VISakura/KERNEL-X00T
+git clone https://$GITHUB_TOKEN@github.com/Takanashi-Hikari/KERNEL-X00T
 cd KERNEL-X00T
-curl https://raw.githubusercontent.com/VISakura/myscripts/master/MASTER.sh | bash
+curl https://raw.githubusercontent.com/Takanashi-Hikari/myscripts/master/MASTER.sh | bash
