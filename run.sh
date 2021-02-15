c1abdedccec31a3d5e30ecf7c866226caaba2c83
ビクセル <vcyzscape@gmail.com>
2021-02-15 15:56:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 git clone https://$GITHUB_TOKEN@github.com/vcyzteen/TEA-PROJECT -b TEA-OLDCAM
 cd TEA-PROJECT
-curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/PROTON.sh | bash
+curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/CLANG11.sh | bash
