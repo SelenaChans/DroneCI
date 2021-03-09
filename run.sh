1b8f5c554e341701ebdb935b334e360c3870a0fc
ビクセル <vcyzscape@gmail.com>
2021-03-09 13:49:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://$GITHUB_TOKEN@github.com/vcyzteen/TEA-PROJECT -b BB
+git clone https://$GITHUB_TOKEN@github.com/vcyzteen/TEA-PROJECT -b NORMAL
 cd TEA-PROJECT
-curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/MASTER.sh | bash
+curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/PROTON.sh | bash
