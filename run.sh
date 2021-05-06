6f6858a24da2d1ae1f5aee63c87aa20cc5206787
Vixel <vcyzscape@gmail.com>
2021-05-07 03:33:15 +0700

    @Baka-Project
    * Baka
    * Baka
    * Baka

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/vcyzteen/baka
+git clone https://github.com/vcyzteen/baka -b lineage-18.1-4.19
 cd baka
 curl https://raw.githubusercontent.com/vcyzteen/myscripts/master/MASTER.sh | bash
