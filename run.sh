dbd6b14a43963fae92fe6cdde810e6217e6b0bd8
Razs <73821326+rasenss@users.noreply.github.com>
2021-07-06 20:09:08 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/rasenss/kernel_xiaomi_ginkgo-lo -b R
-cd kernel_xiaomi_ginkgo-lo
+git clone https://github.com/rasenss/kernel_xiaomi_ginkgo -b R
+cd kernel_xiaomi_ginkgo
 curl https://raw.githubusercontent.com/rasenss/myscripts/kernel.sh | bash
